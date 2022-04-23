8a7bc83d130a7a13308388a0d900181b4e09be0b
temesgenzewude <temesgenzewude21@gmail.com>
2022-04-23 14:09:04 +0300

    domino pilling

---
+++ week_01/domino_pilling.py
@@ -0,0 +1,11 @@
+
+
+def printMaxDominoes(m, n):
+    maxDominoes=(m * n )//2
+    print(maxDominoes)
+
+def main():
+    m=eval(input("Enter m: "))
+    n=eval(input("Enter n: "))
+    printMaxDominoes(m,n)
+main()
