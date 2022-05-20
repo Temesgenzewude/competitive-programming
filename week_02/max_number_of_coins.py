e4be907968e53a0deea4b54bd6f0461355f7d558
temesgenzewude <temesgenzewude21@gmail.com>
2022-05-20 19:16:05 +0300

    max number of coins

---
+++ week_02/max_number_of_coins.py
@@ -0,0 +1,67 @@
+'''
+There are 3n piles of coins of varying size,
+you and your friends will take piles of coins as follows:
+
+In each step, you will choose any 3 piles of coins (not necessarily consecutive).
+Of your choice, Alice will pick the pile with the maximum number of coins.
+You will pick the next pile with the maximum number of coins.
+Your friend Bob will pick the last pile.
+Repeat until there are no more piles of coins.
+Given an array of integers piles where piles[i] is the number of coins in the ith pile.
+Return the maximum number of coins that you can have.
+
+Example 1:
+Input: piles = [2,4,1,2,7,8]
+Output: 9
+Explanation: Choose the triplet (2, 7, 8), Alice Pick the pile with 8 coins, you
+the pile with 7 coins and Bob the last one.
+Choose the triplet (1, 2, 4), Alice Pick the pile
+ with 4 coins, you the pile with 2 coins and Bob the last one.
+The maximum number of coins which you can have are: 7 + 2 = 9.
+On the other hand if we choose this arrangement (1, 2, 8), (2, 4, 7)
+ you only get 2 + 4 = 6 coins which is not optimal.
+Example 2:
+Input: piles = [2,4,5]
+Output: 4
+Example 3:
+Input: piles = [9,8,7,6,5,1,2,3,4]
+Output: 18
+
+Constraints:
+3 <= piles.length <= 105
+piles.length % 3 == 0
+1 <= piles[i] <= 104
+'''
+
+
+class Solution(object):
+    def maxCoins(self, piles):
+        """
+        :type piles: List[int]
+        :rtype: int
+        """
+        result = 0
+        turns = len(piles) / 3
+        my_index = len(piles) - 2
+        piles.sort()
+        while turns > 0:
+            result += piles[my_index]
+            my_index -= 2
+            turns -= 1
+        return result
+
+
+def main():
+    sol = Solution()
+    piles = [2, 4, 1, 2, 7, 8]
+    piles2 = [2, 4, 5]
+    piles3 = [9, 8, 7, 6, 5, 1, 2, 3, 4]
+    result1 = sol.maxCoins(piles)
+    result2 = sol.maxCoins(piles2)
+    result3 = sol.maxCoins(piles3)
+    print(result1)
+    print(result2)
+    print(result3)
+
+
+main()
