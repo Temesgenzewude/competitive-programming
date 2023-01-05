7be14009056df31131aee857324fae1a8e83dc1b
Temesgen Zewude <90067770+Temesgenzewude@users.noreply.github.com>
2023-01-05 12:21:43 +0300

    Time: 604 ms (37.70%), Space: 25.3 MB (78.75%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -4,7 +4,37 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         
-        for i in range(k):
-            last= nums.pop()
-            nums.insert(0, last)
+        # for i in range(k):
+        #     last= nums.pop()
+        #     nums.insert(0, last)
+        
+        
+        # rotate the last k element
+        
+        k = k % len(nums)
+            
+        left= len(nums)-k
+        right= len(nums)-1
+        while left < right:
+            nums[left], nums[right] = nums[right], nums[left]
+            left +=1
+            right-=1
+            
+        # rotate the first n-k elements
+        left= 0
+        right= len(nums)-k-1
+        while left < right:
+            nums[left], nums[right]= nums[right], nums[left]
+            left+=1
+            right-=1
+            
+        # rotate the entire array
+        left =0
+        right= len(nums)-1
+        while left < right:
+            nums[left], nums[right]= nums[right], nums[left]
+            left+=1
+            right-=1
+        
+        
         
