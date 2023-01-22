58b1f0fc6c1fc44d2b25b6299aa655e1f400d9c6
Temesgen Zewude <90067770+Temesgenzewude@users.noreply.github.com>
2023-01-23 00:35:12 +0300

    Time: 1938 ms (31.79%), Space: 29.1 MB (5.96%) - LeetHub

---
+++ 2110-number-of-smooth-descent-periods-of-a-stock/2110-number-of-smooth-descent-periods-of-a-stock.py
@@ -0,0 +1,16 @@
+class Solution:
+    def getDescentPeriods(self, prices: List[int]) -> int:
+        n=len(prices)
+        dp=[1]
+        
+        for x, y in zip(prices, prices[1:]):
+            if x == y+1:
+                dp.append(dp[-1] +1)
+            else:
+                dp.append(1)
+        return sum(dp)
+            
+        
+        
+        
+        
