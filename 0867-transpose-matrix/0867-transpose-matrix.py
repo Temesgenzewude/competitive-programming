c84993004bc8712cc90253a9c0f350022ec446e2
Temesgen Zewude <90067770+Temesgenzewude@users.noreply.github.com>
2023-03-03 22:36:14 +0300

    Time: 75 ms (66.47%), Space: 14.9 MB (12.66%) - LeetHub

--- 0867-transpose-matrix/0867-transpose-matrix.py
+++ 0867-transpose-matrix/0867-transpose-matrix.py
@@ -3,9 +3,9 @@ class Solution:
         rows=len(matrix)
         cols=len(matrix[0])
         transpose=[[0 for i in range(rows)]  for j in range(cols)] 
-        for i in range(cols):
-            for j in range(rows):
-                transpose[i][j]=matrix[j][i]
+        for i in range(rows):
+            for j in range(cols):
+                transpose[j][i]=matrix[i][j]
         
         return transpose
 
