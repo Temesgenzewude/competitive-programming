e72c6946424692990413700f7445cd275053a4d7
Temesgen Zewude <90067770+Temesgenzewude@users.noreply.github.com>
2023-03-06 21:59:33 +0300

    Time: 48 ms (83.06%), Space: 14.8 MB (30.25%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -1,32 +1,56 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        lo=0
-        hi= len(nums)-1
-        mid = (lo + hi)//2
         
-        mid_val= nums[mid]
         
-        if target > nums[hi]:
-            return hi+1
-        if target < nums[lo]:
-            return lo
         
-        while lo <= hi:
+        def isGreaterOrEqual(mid_num):
+            if mid_num >=target:
+                return True
+            else:
+                return False
+        low=-1
+        high=len(nums)
+        
+        while high > low+1:
+            mid= low + (high-low)//2
+            
+            mid_num=nums[mid]
+            
+            if isGreaterOrEqual(mid_num):
+                high=mid
+            else:
+                low=mid
+        
+        return high
+        
+        
+#         lo=0
+#         hi= len(nums)-1
+#         mid = (lo + hi)//2
+        
+#         mid_val= nums[mid]
+        
+#         if target > nums[hi]:
+#             return hi+1
+#         if target < nums[lo]:
+#             return lo
+        
+#         while lo <= hi:
             
         
             
-            if mid_val== target:
-                return mid
-            elif mid_val > target:
-                hi= mid - 1
-                if nums[hi] < target:
-                    return hi+1
-            elif mid_val < target:
-                lo= mid +1
-                if nums[lo] > target:
-                    return lo
-            mid = (lo + hi)//2
-            mid_val= nums[mid]
+#             if mid_val== target:
+#                 return mid
+#             elif mid_val > target:
+#                 hi= mid - 1
+#                 if nums[hi] < target:
+#                     return hi+1
+#             elif mid_val < target:
+#                 lo= mid +1
+#                 if nums[lo] > target:
+#                     return lo
+#             mid = (lo + hi)//2
+#             mid_val= nums[mid]
       
                 
                 
