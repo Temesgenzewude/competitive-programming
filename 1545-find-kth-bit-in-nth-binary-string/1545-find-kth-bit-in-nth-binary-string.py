0dac663cd452126612c8384ac35786f96d739560
Temesgen Zewude <90067770+Temesgenzewude@users.noreply.github.com>
2023-03-15 14:28:49 +0300

    Time: 21 ms (99.75%), Space: 13.9 MB (88.18%) - LeetHub

--- 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.py
+++ 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.py
@@ -1,24 +1,39 @@
 class Solution:
     def findKthBit(self, n: int, k: int) -> str:
         
-        def invert(s):
-            newS=list(s)
-            for i in range(len(newS)):
-                if newS[i]=="0":
-                    newS[i]="1"
-                elif newS[i]=="1":
-                    newS[i]="0"
-            return "".join(newS)
-        
-        def helper(n):
+        def helper(n,k):
             if n==1:
                 return "0"
-            si=helper(n-1)
             
-            return si + "1" + (invert(si)[::-1])
+        
+            if k < (pow(2,n)//2):
+                return str(helper(n-1, k))
+            elif k > pow(2,n)//2:
+                return str(1-int(helper(n-1, pow(2,n)-k)))
+            elif k==pow(2,n)//2:
+                return "1"
+            
+        return helper(n,k)
+            
+        
+#         def invert(s):
+#             newS=list(s)
+#             for i in range(len(newS)):
+#                 if newS[i]=="0":
+#                     newS[i]="1"
+#                 elif newS[i]=="1":
+#                     newS[i]="0"
+#             return "".join(newS)
+        
+#         def helper(n):
+#             if n==1:
+#                 return "0"
+#             si=helper(n-1)
+            
+#             return si + "1" + (invert(si)[::-1])
         
         
-        result= helper(n)
+#         result= helper(n)
         
-        return result[k-1]
+#         return result[k-1]
         
