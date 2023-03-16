7afa4c577d909bbf3e08e7c8fa42f7cfb71a77ec
Temesgen Zewude <90067770+Temesgenzewude@users.noreply.github.com>
2023-03-16 11:45:40 +0300

    Time: 82 ms (24.92%), Space: 14 MB (22.36%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.py
@@ -0,0 +1,53 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        
+        result=[]
+        n=len(candidates)
+        
+        def backtrack(candidate, combination=[]):
+            
+            if sum(combination)==target:
+                
+                result.append(combination.copy())
+                return 
+            
+            if candidate >=n or sum(combination) > target: 
+                return
+            
+            
+            
+                
+            
+            combination.append(candidates[candidate])
+            backtrack(candidate, combination)
+            combination.pop()
+            backtrack(candidate+1, combination)
+#             
+            
+#             if candidate >=n: 
+#                 return
+            
+#             if sum(combination)==target:
+#                 print(sum(combination))
+#                 result.append(combination.copy())
+#                 combination.pop()
+#                 backtrack(candidate+1, combination)
+#             elif sum(combination) > target:
+#                 combination.pop()
+#                 backtrack(candidate+1, combination)
+#             elif sum(combination)< target:
+#                 combination.append(candidates[candidate])
+#                 backtrack(candidate, combination)
+                
+        
+        backtrack(0)
+        
+        return result
+            
+            
+            
+                
+                
+        
+        
+        
