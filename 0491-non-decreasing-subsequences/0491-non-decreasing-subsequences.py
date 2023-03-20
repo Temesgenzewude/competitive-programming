8d2b33a626d54ddbb87930862224b3acf589234f
Temesgen Zewude <90067770+Temesgenzewude@users.noreply.github.com>
2023-03-20 19:09:48 +0300

    Time: 236 ms (70.50%), Space: 26.8 MB (7.23%) - LeetHub

--- 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.py
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.py
@@ -4,34 +4,54 @@ class Solution:
         result=set()
         
         def helper(idx, seq):
+            
             if len(seq)>=2:
                 result.add(tuple(seq))
-        
             if idx >=len(nums):
                 return
             
-           
-                
-                
-            if (seq and nums[idx] >=seq[-1]) or (not seq):
-                seq.append(nums[idx])
-                helper(idx+1, seq)
+            for i in range(idx, len(nums)):
+                if seq and seq[-1] > nums[i]:
+                    continue
+                seq.append(nums[i])
+                helper(i+1, seq)
                 seq.pop()
-                
         
-            helper(idx+1,seq)
-                
-           
-            
-            
-        
+        result=set()
         helper(0,[])
-        solution=[]
-        
-        
+        solution = []
         for res in result:
             solution.append(list(res))
+        
         return solution
+#             if len(seq)>=2:
+#                 result.add(tuple(seq))
+        
+#             if idx >=len(nums):
+#                 return
+            
+           
+                
+                
+#             if (seq and nums[idx] >=seq[-1]) or (not seq):
+#                 seq.append(nums[idx])
+#                 helper(idx+1, seq)
+#                 seq.pop()
+                
+        
+#             helper(idx+1,seq)
+                
+           
+            
+            
+        
+#         helper(0,[])
+#         solution=[]
+        
+        
+#         for res in result:
+#             solution.append(list(res))
+#         return solution
                 
                 
             
