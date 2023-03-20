fc75a44c00b569596255cf8065a55ba3a9177f70
Temesgen Zewude <90067770+Temesgenzewude@users.noreply.github.com>
2023-03-20 19:08:04 +0300

    Time: 260 ms (34.48%), Space: 26.8 MB (7.23%) - LeetHub

--- 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.py
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.py
@@ -1,23 +1,44 @@
 class Solution:
     def findSubsequences(self, nums: List[int]) -> List[List[int]]:
         
+        result=set()
+        
         def helper(idx, seq):
             if len(seq)>=2:
                 result.add(tuple(seq))
+        
             if idx >=len(nums):
                 return
             
-            for i in range(idx, len(nums)):
-                if seq and seq[-1] > nums[i]:
-                    continue
-                seq.append(nums[i])
-                helper(i+1, seq)
+           
+                
+                
+            if (seq and nums[idx] >=seq[-1]) or (not seq):
+                seq.append(nums[idx])
+                helper(idx+1, seq)
                 seq.pop()
+                
         
-        result=set()
+            helper(idx+1,seq)
+                
+           
+            
+            
+        
         helper(0,[])
-        solution = []
+        solution=[]
+        
+        
         for res in result:
             solution.append(list(res))
-        
         return solution
+                
+                
+            
+            
+                
+            
+            
+            
+                
+        
