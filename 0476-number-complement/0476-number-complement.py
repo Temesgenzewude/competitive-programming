5b81c222197df0bcb27a43d36386dfa253e53d0c
Temesgen Zewude <90067770+Temesgenzewude@users.noreply.github.com>
2023-03-30 19:20:42 +0300

    Time: 37 ms (21.60%), Space: 13.8 MB (91.06%) - LeetHub

--- 0476-number-complement/0476-number-complement.py
+++ 0476-number-complement/0476-number-complement.py
@@ -7,16 +7,10 @@ class Solution:
         
         count=0
         
-        
-        
         while new !=0:
             count+=1
             new>>=1
        
-            
-    
-
-        
         for i in range(count):
             num^=mask
             mask<<=1
