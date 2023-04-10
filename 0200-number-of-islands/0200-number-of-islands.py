94f80447b0edd7b3f174e81a2e2b7f27bdec2b19
Temesgen Zewude <90067770+Temesgenzewude@users.noreply.github.com>
2023-04-10 21:45:55 +0300

    Time: 299 ms (67.37%), Space: 16.4 MB (62.52%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -0,0 +1,39 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        
+        count=0
+        visited=set() 
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        
+        def isInboundAndValid(row, col):
+            
+            return 0 <= row < len(grid) and 0 <= col < len(grid[0]) and grid[row][col]=="1"
+        
+        def dfs(row, col):
+            
+            grid[row][col]="0"
+           
+            # if isInboundAndValid(row, col) and (row, col) not in visited:
+            #     visited.add((row, col))
+            
+            for row_change, col_change in directions:
+                new_row=row+row_change
+                new_col=col + col_change
+
+                if 0 <= new_row < len(grid) and 0 <= new_col < len(grid[0]) and grid[new_row][new_col]=="1":
+                    dfs(new_row, new_col)
+                    
+            
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j]=="1":
+                    
+                    dfs(i,j)
+                    count += 1
+        return count
+                    
+                    
+                    
+                    
+                    
+        
