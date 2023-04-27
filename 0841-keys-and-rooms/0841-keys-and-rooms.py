542e453c3c90e34942ef02b49cc877dc5778c957
Temesgen Zewude <90067770+Temesgenzewude@users.noreply.github.com>
2023-04-27 18:13:30 +0300

    Time: 94 ms (5.27%), Space: 17 MB (10.24%) - LeetHub

---
+++ 0841-keys-and-rooms/0841-keys-and-rooms.py
@@ -0,0 +1,38 @@
+class Solution:
+    def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+        
+        
+        graph=defaultdict(list)
+        
+        for room in range(len(rooms)):
+            for key in rooms[room]:
+                graph[room].append(key)
+        
+        print(graph)
+                
+        
+        def bfs(graph, node):
+            visited=set([node])
+            que=deque([node])
+            
+            
+            while que:
+                node=que.popleft()
+                
+                for neighbour in graph[node]:
+                    if neighbour not in visited:
+                        visited.add(neighbour)
+                        que.append(neighbour)
+            
+            return len(visited)==len(rooms)
+            
+                        
+        return bfs(graph, 0)
+        
+        
+                        
+        
+        return -1
+            
+                        
+        
