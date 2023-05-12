9b361df6b85532ba7adaf69d9ece27eea74c8acd
Temesgen Zewude <90067770+Temesgenzewude@users.noreply.github.com>
2023-05-12 21:31:30 +0300

    Time: 1133 ms (65.02%), Space: 65.5 MB (48.92%) - LeetHub

--- 1743-restore-the-array-from-adjacent-pairs/1743-restore-the-array-from-adjacent-pairs.py
+++ 1743-restore-the-array-from-adjacent-pairs/1743-restore-the-array-from-adjacent-pairs.py
@@ -6,17 +6,14 @@ class Solution:
         for adj1, adj2 in adjacentPairs:
             graph[adj1].append(adj2)
             graph[adj2].append(adj1)
-        extremes=[]
-        
-     
             
+        extremes=[]
+       
         for key in graph:
             if len(graph[key])==1:
                 extremes.append(key)
                 break
-                
-                    
-        
+         
         queue=deque([extremes[0]])
         
         visited=set([extremes[0]])
@@ -34,18 +31,4 @@ class Solution:
         
         return result
                 
-            
-            
-        
       
-                
-            
-        
-        # return [1,2,3,4]
-            
-        
-        
-        
-        
-        
-        
