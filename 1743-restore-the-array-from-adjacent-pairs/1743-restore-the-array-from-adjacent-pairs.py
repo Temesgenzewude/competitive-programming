6e9a513f6c72d794bb9ce447f37e4cef1fad513f
Temesgen Zewude <90067770+Temesgenzewude@users.noreply.github.com>
2023-05-12 21:25:49 +0300

    Time: 1132 ms (65.63%), Space: 65.2 MB (51.08%) - LeetHub

---
+++ 1743-restore-the-array-from-adjacent-pairs/1743-restore-the-array-from-adjacent-pairs.py
@@ -0,0 +1,51 @@
+class Solution:
+    def restoreArray(self, adjacentPairs: List[List[int]]) -> List[int]:
+        
+        graph=defaultdict(list)
+        
+        for adj1, adj2 in adjacentPairs:
+            graph[adj1].append(adj2)
+            graph[adj2].append(adj1)
+        extremes=[]
+        
+     
+            
+        for key in graph:
+            if len(graph[key])==1:
+                extremes.append(key)
+                if len(extremes)==2:
+                    break
+                    
+        
+        queue=deque([extremes[0]])
+        
+        visited=set([extremes[0]])
+        result=[extremes[0]]
+        
+        while queue:
+            curr=queue.popleft()
+            
+            for neighbor in graph[curr]:
+                if neighbor not in visited:
+                    result.append(neighbor)
+                    visited.add(neighbor)
+                    queue.append(neighbor)
+                    
+        
+        return result
+                
+            
+            
+        
+      
+                
+            
+        
+        # return [1,2,3,4]
+            
+        
+        
+        
+        
+        
+        
