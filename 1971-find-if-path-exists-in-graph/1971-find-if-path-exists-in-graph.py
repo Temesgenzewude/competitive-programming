0abe1422ce57f7706da5c755e451f193b2050646
Temesgen Zewude <90067770+Temesgenzewude@users.noreply.github.com>
2023-05-19 18:13:02 +0300

    Time: 2178 ms (43.41%), Space: 108.7 MB (54.32%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -1,15 +1,47 @@
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
         
-        graph={i:[] for i in range(n)}
+        self.reps={i:i for i in range(n) }
+        self.sizes={i:1 for i in range(n)}
+
+        def find(x):
+            parent=self.reps[x]
+
+            while parent !=self.reps[parent]:
+                parent=self.reps[parent]
+
+
+            while x !=self.reps[x]:
+                self.reps[x]=parent
+                x=self.reps[x]
+
+
+            return parent
+
+        def union(u,v):
+            uRep=find(u)
+            vRep=find(v)
+
+            if self.sizes[uRep] > self.sizes[vRep]:
+                self.reps[vRep]=uRep
+                self.sizes[uRep]+=self.sizes[vRep]
+            else:
+                self.reps[uRep]=vRep
+                self.sizes[vRep]+=self.sizes[uRep]
+
+        for u,v in edges:
+            union(u,v)
+    
+        return find(source)==find(destination)
         
+        
+       
+        graph={i:[] for i in range(n)}
+
         for u,v in edges:
             graph[u].append(v)
             graph[v].append(u)
-            
-            
-        
-        
+
         def bfs(node):
             if node==destination:
                 return True
@@ -29,19 +61,10 @@ class Solution:
             
             return False
         
-        
-        return bfs(source)
+            
+            
     
-                        
             
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
+    
         
