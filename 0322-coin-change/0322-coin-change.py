caec51551bd60ceb079d523c1e7edfb930f433c4
Temesgen Zewude <90067770+Temesgenzewude@users.noreply.github.com>
2023-06-01 19:12:20 +0300

    Time: 1519 ms (36.14%), Space: 16.7 MB (53.81%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -4,9 +4,10 @@ class Solution:
         
         memo={}
         def dp(amount):
+            
             if amount in memo:
                 return memo[amount]
-            
+        
             if amount==0:
                 return 0
             if amount < 0:
@@ -20,9 +21,27 @@ class Solution:
             return memo[amount]
         
         
-        result=dp(amount)
+#         result=dp(amount)
         
-        return result if result !=float("inf") else -1
+#         return result if result !=float("inf") else -1
+        
+        arr=[float("inf") for _ in range(amount+1)]
+        arr[0]=0
+        
+        
+        for i in range(amount+1):
+            ans=float("inf")
+            # print(i)
+            for coin in coins:
+                if i-coin >= 0:
+
+                    ans=min(ans, arr[i-coin]+1)
+            arr[i]=min(ans, arr[i])
+       
+            
+        return arr[-1] if arr[-1] !=float("inf") else -1
+        
+            
         
         
         
