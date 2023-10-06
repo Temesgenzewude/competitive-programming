1e5c24b837996dddd4168ffabbf49747a5b83464
Temesgen Zewude <90067770+Temesgenzewude@users.noreply.github.com>
2023-10-06 22:24:15 +0300

    Update negative_cycle_bellman_ford.py

--- negative_cycle_bellman_ford.py
+++ negative_cycle_bellman_ford.py
@@ -25,29 +25,19 @@ def negativeCycle():
             src,dst,length = edge
             parent[dst] = src
             if  distance[src] + length < distance[dst]:
-                distance[dst] = distance[src] + length
-                
-    
-    
-    print('graph',graph) 
-    print('distance',distance)
-    print('parent',parent)   
+                distance[dst] = distance[src] + length   
     
     for edge in graph:
         src,dst,length = edge
         if  distance[src] + length < distance[dst]:
             print("YES")
             cycle = [src]
-            print('cycle start', src)
+        
             node = parent[src]
             while node!=-1 and node not in cycle:
-                print('cycle node', node)
-
                 cycle.append(node)
                 node = parent[node]
             cycle.append(node)
-
-            print('cycle end', node)
             print(*cycle[::-1])
             # cycle = cycle[cycle.index(node):]
             
