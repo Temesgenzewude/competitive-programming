06e5dff7d06ac17d1e84571ac39e21e8ac8eda27
Temesgen Zewude <90067770+Temesgenzewude@users.noreply.github.com>
2023-10-23 06:44:52 +0300

    Time: 49 ms (86.05%), Space: 17.3 MB (61.31%) - LeetHub

---
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
@@ -0,0 +1,17 @@
+class Solution:
+    def reverseVowels(self, s: str) -> str:
+        s=list(s)
+        n=len(s)
+        left=0
+        right=n-1
+        vowels=set('AEIOUaeiou')
+        while left<right:
+            while left<right and s[left] not in vowels:
+                left+=1
+            while left<right and s[right] not in vowels:
+                right-=1
+            s[left],s[right]=s[right],s[left]
+            left+=1
+            right-=1
+        s=''.join(s)
+        return s
